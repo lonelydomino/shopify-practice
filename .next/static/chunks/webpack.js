1dafad13ec133133d3b178a841f6f42b7e2e2b6c
Milo <zeroeclipse@live.com>
2021-12-06 13:08:54 -0800

    Adding packages and git ignore file.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b1e4557f23c8564"; }
+/******/ 		__webpack_require__.h = function() { return "4d6eadcde46bac67"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
